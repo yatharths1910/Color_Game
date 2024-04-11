a095704449bf553c6f5a0a4e0b0e4a83d28e54e2
yatharths1910 <159349690+yatharths1910@users.noreply.github.com>
2024-04-12 10:30:16 +1200

    Add files via upload
    Color lists in name and hex, font size and font color

---
+++ 01_list_demo.py
@@ -0,0 +1,11 @@
+some_colors = [
+    ['aliceblue', '#F0F8FF', '19', 'black'],
+    ['antiquewhite', '#FAEBD7','20', 'black'],
+    ['antiquewhite1', '#FFEFDB','20', 'black'],
+    ['antiquewhite2', '#EEDFCC','19', 'black'],
+    ['antiquewhite3', '#CDC0B0','16', 'black'],
+    ['antiquewhite4', '#8B8378','11', 'white']
+]
+
+for item in some_colors:
+    print(item[0])
